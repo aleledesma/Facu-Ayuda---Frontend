cf0232ecb539c213aab81082cfd9ededcb284e1f
aleledesma <aleledesma1212@gmail.com>
2024-05-06 20:59:58 -0300

    style: modify error alert component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import ErrorAlert from "@/components/ui/ErrorAlert"
 import Image from "next/image"
 
 export default function Home() {
-  return <div>main </div>
+  return <div>main</div>
 }
--- src/components/ui/ErrorAlert.tsx
+++ src/components/ui/ErrorAlert.tsx
@@ -8,7 +8,7 @@ export default function ErrorAlert({
   errorMessage,
   className,
 }: ErrorAlertProps) {
-  const styles = `alert alert-error ${className}`
+  const styles = `bg-error flex gap-4 items-center py-5 px-4 text-gray-900 lg:w-[900px] md:w-[650px] w-[90%] md:text-base text-sm rounded-xl ${className}`
   return (
     <div role="alert" className={styles}>
       <ErrorCrossIcon />
